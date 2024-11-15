57b3ca5d224b88099ab124a7948c0665c53ce1e8
sanyi8 <sanyitelo@gmail.com>
2024-11-15 16:33:10 +0000

    added clear()

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from turtle import Turtle, Screen
 
 tim = Turtle()
+tim.speed(0)
 #tim.shape("turtle")
 screen = Screen()
 
 def move_forwards():
-    tim.forward(10)
+    tim.forward(50)
 
 def move_left():
     tim.left(30)
@@ -16,11 +17,20 @@ def move_right():
 def move_back():
     tim.backward(10)
 
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
 # keyboard setup
 screen.onkey(key = "w", fun = move_forwards)
 screen.onkey(key = "a", fun = move_left)
 screen.onkey(key = "s", fun = move_back)
 screen.onkey(key = "d", fun = move_right)
+screen.onkey(key = "c", fun = clear)
+
 
 
 screen.listen()
