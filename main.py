87ad737c0f8cece78b7300b40c43c1a4f023d53f
sanyi8 <sanyitelo@gmail.com>
2024-11-15 15:47:50 +0000

    etch a sketch

---
+++ main.py
@@ -0,0 +1,34 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+#tim.shape("turtle")
+screen = Screen()
+
+
+def move_forwards():
+    tim.forward(10)
+
+def move_left():
+    tim.left(30)
+
+def move_right():
+    tim.right(30)
+
+def move_back():
+    tim.backward(10)
+
+# keyboard setup
+screen.onkey(key = "w", fun = move_forwards)
+screen.onkey(key = "a", fun = move_left)
+screen.onkey(key = "s", fun = move_back)
+screen.onkey(key = "d", fun = move_right)
+
+
+screen.listen()
+screen.bgcolor("azure3")
+# onkey method listens for space and than call move_forward()
+screen.onkey(key="space", fun=move_forwards)
+
+
+# keep screen open
+screen.mainloop()
